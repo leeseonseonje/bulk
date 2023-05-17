247b99f8478f21d96e8a68a72f2cf6fe51086b5d
seonje <leeseonje9323@gmail.com>
2023-05-17 23:44:06 +0900

    pickle파일 경로 프로젝트 루트로 변경

--- src/main/app/init/db_connection_dto.py
+++
@@ -1,8 +0,0 @@
-class DBConnectionDto:
-    def __init__(self, dbms, host, port, user, password, db):
-        self.dbms = dbms
-        self.host = host
-        self.port = port
-        self.user = user
-        self.password = password
-        self.db = db
