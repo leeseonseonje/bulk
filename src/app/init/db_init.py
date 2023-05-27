e983e8694401d708261e6aef3a79dbf45a1097e3
seonje <leeseonje9323@gmail.com>
2023-05-27 22:04:34 +0900

    init할 경우 잘못된 정보를 입력해도 pickle에 save되는 버그 수정

--- src/app/init/db_init.py
+++ src/app/init/db_init.py
@@ -1,14 +1,21 @@
+import sys
+
 import typer
-from src.app.init.db_connection import get_connection
+from src.app.init.db_connection import DBConnection
 from src.app.init.db_connection_config import DBConnectionConfig
 from src.app.repository.pickle_db_info_repository import save_db_info
 
 
 def db_init(dbms):
-    connection_dto = host_port_user_password_db(dbms)
-    save_db_info(connection_dto)
-    get_connection()
-    print('connect success!!')
+    db_info = host_port_user_password_db(dbms)
+    try:
+        connect_db = DBConnection(db_info.host, db_info.port, db_info.user, db_info.password, db_info.db)
+        getattr(connect_db, db_info.dbms)()
+        save_db_info(db_info)
+        print('connect success!!')
+    except:
+        print('connect failed!!')
+        sys.exit(1)
 
 
 def host_port_user_password_db(dbms):
